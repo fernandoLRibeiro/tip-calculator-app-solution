dbcacb812ebc3fe54263a078bbe70f3880ccb881
Fernando L. Ribeiro <fernandolribeiro@protonmail.com>
2022-05-15 14:36:04 -0300

    Implement theme and global styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyles/Global";
+
+const theme = {
+  colors: {
+    primary: {
+      strongCyan: "hsl(172, 67%, 45%)",
+    },
+    neutral: {
+      veryDarkCyan: "hsl(183, 100%, 15%)",
+      darkGrayisCyan: "hsl(186, 14%, 43%)",
+      grayishCyan: "hsl(184, 14%, 56%)",
+      lightGrayishCyan: "hsl(185, 41%, 84%)",
+      veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
   );
 }
 
